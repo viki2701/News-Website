d7cfe386c74bec45c9a4313ec7b333b4be82769e
viki2701 <viki2727eee@gmail.com>
2023-01-16 15:00:09 +0530

    16.01.2023

--- news/src/app/app.module.ts
+++ news/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainbodyComponent } from './mainbody/mainbody.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainbodyComponent
   ],
   imports: [
     BrowserModule,
---
+++ news/src/app/mainbody/mainbody.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mainbody',
+  templateUrl: './mainbody.component.html',
+  styleUrls: ['./mainbody.component.scss']
+})
+export class MainbodyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  
+}
